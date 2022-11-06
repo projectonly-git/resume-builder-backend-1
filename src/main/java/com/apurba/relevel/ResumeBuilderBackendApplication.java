ec6be67f3c57e85663489babfd0173488a8f73d4
projectonly-git <117011228+projectonly-git@users.noreply.github.com>
2022-11-06 23:11:16 +0530

    upload

---
+++ src/main/java/com/apurba/relevel/ResumeBuilderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.apurba.relevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeBuilderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeBuilderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apurba/relevel/ResumeBuilderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apurba.relevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBuilderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
