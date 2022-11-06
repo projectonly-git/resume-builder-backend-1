f244e976f1282bb70eec9af52fd96974ade19b82
projectonly-git <117011228+projectonly-git@users.noreply.github.com>
2022-11-06 23:21:17 +0530

    added the model

---
+++ src/main/java/com/apurba/relevel/model/Education.java
@@ -0,0 +1,79 @@
+package com.apurba.relevel.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity(name="education")
+public class Education {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer eduid;
+    private String course;
+    private String institution;
+    private String startyear;
+    private String endyear;
+    private String marks;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Resume resume;
+
+    public Integer getEduid() {
+        return eduid;
+    }
+
+    public void setEduid(Integer eduid) {
+        this.eduid = eduid;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getStartyear() {
+        return startyear;
+    }
+
+    public void setStartyear(String startyear) {
+        this.startyear = startyear;
+    }
+
+    public String getEndyear() {
+        return endyear;
+    }
+
+    public void setEndyear(String endyear) {
+        this.endyear = endyear;
+    }
+
+    public String getMarks() {
+        return marks;
+    }
+
+    public void setMarks(String marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Education{" +
+                "course='" + course + '\'' +
+                ", institution='" + institution + '\'' +
+                ", startyear='" + startyear + '\'' +
+                ", endyear='" + endyear + '\'' +
+                ", marks='" + marks + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/apurba/relevel/model/User.java
@@ -0,0 +1,58 @@
+package com.apurba.relevel.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity(name="users")
+public class User {
+    @Id
+    private String emailid;
+    private String password;
+    private String username;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy="user")
+    private List<Resume> resumelist;
+
+    public String getEmailid() {
+        return emailid;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Resume> getResumelist() {
+        return resumelist;
+    }
+
+    public void setResumelist(List<Resume> resumelist) {
+        this.resumelist = resumelist;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "emailid='" + emailid + '\'' +
+                ", username='" + username + '\'' +
+                ", resumelist=" + resumelist +
+                '}';
+    }
+}
